61e6002db7d4773588ce06245dcb3272ed7316eb
misael <misael111998@gmail.com>
2019-03-26 15:13:45 -0600

    Creando la verificacion del login

--- app/src/main/java/com/favio/unioncanina/InicioSesionActivity.java
+++ app/src/main/java/com/favio/unioncanina/InicioSesionActivity.java
@@ -7,6 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.StringReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class InicioSesionActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -28,15 +40,44 @@ public class InicioSesionActivity extends AppCompatActivity implements View.OnCl
         tv_registrame.setOnClickListener(this);
     }
 
+
+
+    public void login(){
+      StringRequest st=new StringRequest(Request.Method.GET,
+              "http://unioncanina.mipantano.com/loginandroid",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if(response.contains("1")){
+                            startActivity(new Intent(getApplicationContext(),InicioActivity.class));
+                        }
+                        Toast.makeText(getApplicationContext(),"Esto es una respuesta"+response,Toast.LENGTH_LONG).show();
+                    }
+                }
+                , new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),"Ahora haber problema",Toast.LENGTH_LONG).show();
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError{
+                Map<String,String> params=new HashMap<>();
+                params.put("usuario",et_correo.getText().toString());
+                params.put("contraseña",et_pwd.getText().toString());
+                return params;
+            }
+
+        };
+        Volley.newRequestQueue(this).add(st);
+    }
     @Override
     public void onClick(View view) {
 
         switch (view.getId()){
 
             case R.id.btn_iniciarSesion:
-                Intent itt_inicioActivity=new Intent(InicioSesionActivity.this, InicioActivity.class);
-                startActivity(itt_inicioActivity);
-                finish();
+                login();
             case R.id.tv_registrame:
                 Intent itt_registroActivity=new Intent(InicioSesionActivity.this, RegistroActivity.class);
                 startActivity(itt_registroActivity);
