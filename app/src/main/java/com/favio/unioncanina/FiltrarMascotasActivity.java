fc2ccf65e0265afcc85ebfc85fe95cca2bdaeb4e
misael <misael111998@gmail.com>
2019-04-19 15:51:06 -0500

    Mensaje

--- app/src/main/java/com/favio/unioncanina/FiltrarMascotasActivity.java
+++ app/src/main/java/com/favio/unioncanina/FiltrarMascotasActivity.java
@@ -3,17 +3,30 @@ package com.favio.unioncanina;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class FiltrarMascotasActivity extends AppCompatActivity implements View.OnClickListener{
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FiltrarMascotasActivity extends AppCompatActivity {
 
     ImageView ic_retroceso;
-    Spinner sp_razaMascotaFiltro, sp_sexoMascotaFiltro, sp_ciudadMascotaFiltro, sp_coloniaMascotaFiltro;
-    EditText et_rasgosMascotaFiltro;
+    Spinner sp_raza, sp_sexo, sp_ciudad;
+    EditText et_rasgo;
     Button btn_aplicarFiltros;
 
     @Override
@@ -22,35 +35,22 @@ public class FiltrarMascotasActivity extends AppCompatActivity implements View.O
         setContentView(R.layout.activity_filtrar_mascotas);
 
         ic_retroceso=findViewById(R.id.ic_retroceso);
-        sp_razaMascotaFiltro=findViewById(R.id.sp_razaMascotaFiltro);
-        sp_sexoMascotaFiltro=findViewById(R.id.sp_sexoMascotaFiltro);
-        sp_ciudadMascotaFiltro=findViewById(R.id.sp_ciudadMascotaFiltro);
-        sp_coloniaMascotaFiltro=findViewById(R.id.sp_coloniaMascotaFiltro);
-        et_rasgosMascotaFiltro=findViewById(R.id.et_rasgosMascotaFiltro);
+        sp_raza=findViewById(R.id.sp_raza);
+        sp_sexo=findViewById(R.id.sp_sexo);
+        sp_ciudad=findViewById(R.id.sp_ciudad);
+        et_rasgo=findViewById(R.id.et_rasgo);
         btn_aplicarFiltros=findViewById(R.id.btn_aplicarFiltros);
 
-        ic_retroceso.setOnClickListener(this);
-        btn_aplicarFiltros.setOnClickListener(this);
+        LoadSexo();
+    }
+    private void LoadSexo(){
+        ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(
+                this,R.array.combo_Sexo ,android.R.layout.simple_list_item_1 );
+        sp_sexo.setAdapter(adapter);
     }
 
-    @Override
-    public void onClick(View view) {
 
-        switch (view.getId()){
+    private void AplicarFiltros(View view){
 
-            case R.id.ic_retroceso:
-                irInicioActivity();
-                break;
-            case R.id.btn_aplicarFiltros:
-                irInicioActivity();
-                break;
-        }
-    }
-
-    public void irInicioActivity(){
-
-        Intent itt_inicioActivity=new Intent(FiltrarMascotasActivity.this, InicioActivity.class);
-        startActivity(itt_inicioActivity);
-        finish();
     }
 }
